fd4908cb3ae7a14d75fab62b168972d697c1452c
juanfed <juanfed12345@gmail.com>
2022-07-20 15:13:05 -0500

    Creo el main de mi ap

---
+++ main.go
@@ -0,0 +1,8 @@
+// creo el archivo main, sera el encargado de encencer mi servio, ya que es el archivo principal que se irá a ejecutar
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello from main ")
+}
