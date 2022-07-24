aad70d50d9861085deb6ab58ffa129d63038d670
juanfed <juanfed12345@gmail.com>
2022-07-24 11:33:47 -0500

    Nuevo comienzo

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
-// creo el archivo main, sera el encargado de encencer mi servio, ya que es el archivo principal que se irá a ejecutar
 package main
 
 import (
-	"v1/server"
+	"fmt"
 )
 
 func main() {
-	// llamo a la funcion que me iniciara el servidor en la ruta que haya designado
-	server.Start()
+	fmt.Print("New Start")
 }
