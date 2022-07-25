98f68f88b8ef5ecccae4a5ebb5be118aec04d4d2
juanfed <juanfed12345@gmail.com>
2022-07-24 22:48:36 -0500

    Insercin de los datos en Mysql y luego en el redis, todo funcionando correctamente

--- repositories/user_redis.go
+++ repositories/user_redis.go
@@ -1,9 +1,9 @@
 package repositories
 
 import (
+	"encoding/json"
 	"fmt"
 	"strconv"
-	"time"
 	"v1/dal"
 	"v1/models"
 
@@ -21,7 +21,12 @@ func NewUserRedisRepositories() *UserRedisRepositories {
 }
 
 func (r *UserRedisRepositories) InsertUser(user models.User) error {
+	usuario := &user
+	usuario.Id = user.Id
+
+	userData, _ := json.Marshal(usuario)
+
 	fmt.Println("Dato que le llega al redis: ", user)
-	err := r.databaseRedis.Set(strconv.Itoa(user.Id), user, 12*time.Hour).Err()
+	err := r.databaseRedis.Set(strconv.Itoa(usuario.Id), userData, 0).Err()
 	return err
 }
