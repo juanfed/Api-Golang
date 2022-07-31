b5df9bca9fc1bf5d08702fbd8032bb0d612aa0fd
juanfed <juanfed12345@gmail.com>
2022-07-31 14:20:48 -0500

    modificaciones pequeñas

--- server/routes.go
+++ server/routes.go
@@ -9,7 +9,7 @@ func StartRoutes() {
 	mysql := dal.NewDatabaseSql()
 	userController := controller.NewController(mysql)
 	e.POST("/user", userController.Set)
-	e.DELETE("/deleteUser/", userController.Delete)
+	e.DELETE("/deleteUser/:id", userController.Delete)
 	e.PUT("/user", userController.Update)
 	e.GET("/user/:id", userController.Get)
 
